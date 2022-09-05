59961e133459e4c3892ffd3de4d993620afd0d11
ArturBystranowski <artur.bystranowski@gmail.com>
2022-09-06 01:40:48 +0200

    update JS and small improvements

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const featureTabs = document.querySelectorAll(".features-tab");
 
 const showAnswer = (e) => {
 	const parent = e.target.closest(".faq-question");
-
 	parent.querySelector(".faq-answer").classList.toggle("active");
 	parent.querySelector("svg path").classList.toggle("active");
 };
@@ -13,32 +12,11 @@ faqHeader.forEach((header) => {
 	header.addEventListener("click", showAnswer);
 });
 
-// for (i = 0; i < featureBtns.length; i++) {
-// 	featureBtns[i].addEventListener("click", (i) => {
-// 		featureBtns.forEach((button) => button.classList.remove("active"));
-// 		featureTabs.forEach((tab) => tab.classList.remove("active"));
-// 		featureBtns[i].classList.add("active");
-// 		featureTabs[i].classList.add("active");
-// 	});
-// }
-
-featureBtns[0].addEventListener("click", (e) => {
-	featureBtns.forEach((button) => button.classList.remove("active"));
-	featureTabs.forEach((tab) => tab.classList.remove("active"));
-	featureBtns[0].classList.add("active");
-	featureTabs[0].classList.add("active");
-});
-
-featureBtns[1].addEventListener("click", (e) => {
-	featureBtns.forEach((button) => button.classList.remove("active"));
-	featureTabs.forEach((tab) => tab.classList.remove("active"));
-	featureBtns[1].classList.add("active");
-	featureTabs[1].classList.add("active");
-});
-
-featureBtns[2].addEventListener("click", (e) => {
-	featureBtns.forEach((button) => button.classList.remove("active"));
-	featureTabs.forEach((tab) => tab.classList.remove("active"));
-	featureBtns[2].classList.add("active");
-	featureTabs[2].classList.add("active");
-});
+for (let i = 0; i < featureBtns.length; i++) {
+	featureBtns[i].addEventListener("click", () => {
+		featureBtns.forEach((button) => button.classList.remove("active"));
+		featureTabs.forEach((tab) => tab.classList.remove("active"));
+		featureBtns[i].classList.add("active");
+		featureTabs[i].classList.add("active");
+	});
+}
