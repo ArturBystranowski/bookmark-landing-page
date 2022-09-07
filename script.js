f951745881c98a2369a2b587f50647638b2cb849
ArturBystranowski <artur.bystranowski@gmail.com>
2022-09-07 23:27:47 +0200

    Add mobile navigation

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const faqHeader = document.querySelectorAll(".faq-question");
 const featureBtns = document.querySelectorAll(".features-select span");
 const featureTabs = document.querySelectorAll(".features-tab");
+const hamIcon = document.querySelector(".icon-ham");
+const closeIcon = document.querySelector(".icon-close");
+const hamMenuLinks = document.querySelectorAll(".menu-ham li");
+const hamMenu = document.querySelector(".menu-ham");
+
+console.log(hamMenuLinks);
+
+const openMenu = () => {
+	hamMenu.classList.add("open");
+	closeIcon.addEventListener("click", closeMenu);
+	hamMenuLinks.forEach((link) => link.addEventListener("click", closeMenu));
+};
+
+const closeMenu = () => {
+	hamMenu.classList.remove("open");
+};
 
 const showAnswer = (e) => {
 	const parent = e.target.closest(".faq-question");
@@ -11,6 +27,8 @@ const showAnswer = (e) => {
 faqHeader.forEach((header) => {
 	header.addEventListener("click", showAnswer);
 });
+
+hamIcon.addEventListener("click", openMenu);
 
 for (let i = 0; i < featureBtns.length; i++) {
 	featureBtns[i].addEventListener("click", () => {
